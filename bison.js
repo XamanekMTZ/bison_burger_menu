896a00fe74b5958e2420b8c5519ee3e6bc59a6c1
Xamanek Martinez - Bloodberry <xamanek@gmail.com>
2022-03-11 14:46:30 -0600

    Commit final

---
+++ bison.js
@@ -0,0 +1,47 @@
+(function() {
+    let nav = document.querySelector('.nav');
+    let navToggle = document.querySelector('.nav-toggle');
+    let navItems = document.querySelectorAll('.nav__item');
+    let navHighlight = document.querySelector('.nav__highlight');
+
+    //console.log(navHighlight);
+
+    function clearHighlightText(e) {
+        e.stopPropagation();
+
+        navHighlight.style.filter = 'blur(20px)';
+        setTimeout(function() {
+            navHighlight.innerText = '';
+        }, 50);
+    }
+
+    nav.addEventListener('mouseover', clearHighlightText);
+
+    function changeHighlightText(e) {
+        e.stopPropagation();
+
+        let newText = this.innerText;
+
+        navHighlight.style.filter = 'blur(20px)';
+        setTimeout(function(){
+            navHighlight.innerText = newText;
+            navHighlight.style.filter = 'blur(20px)';
+        }, 50);
+    }
+
+    for(let i = 0; i < navItems.length; i++) {
+        navItems.item(i).addEventListener('mouseover', changeHighlightText);
+        navItems.item(i).addEventListener('touchstart', changeHighlightText, {passive: true});
+    }
+
+    navToggle.addEventListener('click', function(e) {
+        if(nav.classList.contains('nav--visible')) {
+            nav.classList.remove('nav--visible');
+            navToggle.classList.remove('nav-toggle--toggled');
+        } else {
+            nav.classList.add('nav--visible');
+            navToggle.classList.add('nav-toggle--toggled');
+        }
+    })
+
+})();
